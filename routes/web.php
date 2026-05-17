f43a65c1f39ffa0d6742a70581ccd1279079ad5c
biaoumarsouk <134841390+biaoumarsouk@users.noreply.github.com>
2026-05-17 23:45:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
+Route::get('/migrate-db', function () {
+    Artisan::call('migrate', ["--force" => true]);
+    return "Base de données mise à jour avec succès !";
+});
 Route::get('/', function () {
     return view('welcome');
 });
